dd3744151a49d14fffa030631cc9483cd4e95874
Jorge Fuentes <jsjfuentesj@yahoo.com>
2017-11-12 21:44:44 -0800

    Reorganize file structure and link frontend to backend

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cs130-cinder
 Team "Need One More" from Lab 1A
+
+Running Server
+------------------
+In the main cinder folder run the command:
+
+python server.py runserver -d
+
+The -d makes it restart the server on any file save
+
+To select port:
+  python server.py runserver -d -p [PORT] 
--- cinder/server.py
+++ cinder/server.py
@@ -1,19 +1,32 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, flash, request, redirect
 from flask_script import Manager
+from flask_social import Social
 import pprint
 
 client = MongoClient('mongodb://admin:admin1@ds259255.mlab.com:59255/cinder')
 db = client.cinder
 collection = db.newcollection
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="public", static_folder="src")
 app.config['SECRET_KEY'] = "hard"
+
+#Social Config
+app.config['SOCIAL_FACEBOOK'] = {
+    'consumer_key': '1751621538216434',
+    'consumer_secret': '8052e75bdf98fe8fea0b1c8677544e71'
+}
+app.config['SECURITY_POST_LOGIN'] = '/profile'
+
 manager = Manager(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return "<h1>HELLO WORLD</h1>"
+    return render_template("home.html")
+
+@app.route('/profile')
+def profile():
+    return "<h1>HELLO PROFILE</h1>"
 
 if __name__ == "__main__":
     manager.run()
