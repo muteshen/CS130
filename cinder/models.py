013341c80c6e6de4181833e0dd233f548eeea11d
Jorge Fuentes <jsjfuentesj@yahoo.com>
2017-12-07 21:13:18 -0800

    Add user password hashing

--- cinder/models.py
+++ cinder/models.py
@@ -1,9 +1,10 @@
 from mongoengine import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
 GENDERS = ('M', 'F')
 class Profile(EmbeddedDocument):
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
+    first = StringField(max_length=50)
+    last = StringField(max_length=50)
     gender = StringField(max_length=1, choices=GENDERS)
     age = IntField()
     photo = FileField() #GridGS
@@ -12,11 +13,40 @@ class Profile(EmbeddedDocument):
 class User(Document):
     email = EmailField(required=True)
     new_matches = BooleanField()
-    password = StringField()
+    password_hash = StringField()
     profile = EmbeddedDocumentField(Profile)
     interested_in = StringField(max_length=1, choices=GENDERS)
     answers = ListField(StringField())
     location = StringField()
     connectionid = StringField()
+    cid = IntField()
+    # login = StringField(max_length=80, unique=True)
 
-#now look at mongoengine docs and the first page of the flask book adn figure out how to combine them, i.e how to add functions when getting and setting
+    @property
+    def password(self):
+        raise AttributeError("password unreadable")
+
+    #can just set with User.password = password and it will be auto hashed
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # # Flask-Login integration
+    # def is_authenticated(self):
+    #     return True
+    #
+    # def is_active(self):
+    #     return True
+    #
+    # def is_anonymous(self):
+    #     return False
+    #
+    # def get_id(self):
+    #     return str(self.id)
+    #
+    # @staticmethod
+    # def validate_login(password_hash, password):
+    #     return check_password_hash(password_hash, password)
